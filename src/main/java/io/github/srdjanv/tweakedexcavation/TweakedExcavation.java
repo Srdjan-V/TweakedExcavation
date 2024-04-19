02ff8ea4b30bfc1a0da3a190a00fa29ce3f63d79
SrdjanV <SrdjanV@protonmail.com>
2024-04-19 23:38:48 +0200

    Set dependency on Immersive Engineering should fix #3

--- src/main/java/io/github/srdjanv/tweakedexcavation/TweakedExcavation.java
+++ src/main/java/io/github/srdjanv/tweakedexcavation/TweakedExcavation.java
@@ -17,8 +17,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = TweakedExcavation.MODID,
         version = TweakedExcavation.VERSION,
         name = "Tweaked Excavation",
-        dependencies = "after:crafttweaker;" +
+        dependencies = "required-after:immersiveengineering" +
                 "required-after:tweakedlib@[" + Tags.TWEAKED_LIB_VERSION + ",);" +
+                "after:crafttweaker;" +
                 "after:groovyscript@[" + Tags.GROOVY_SCRIPT_VERSION + ",)")
 public class TweakedExcavation {
 
